4dc342fc0231a14c2481dc628215a43af8d6b78d
a-coman <141495654+a-coman@users.noreply.github.com>
2026-04-28 13:28:46 +0200

    Update base URL in Astro configuration to root

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   devToolbar: { enabled: false },
-  site: 'https://a-coman.github.io', // TODO: change this to your own username
-  base: "/link", // TODO: change this to your repository name
+  base: "/",
   integrations: [
     icon({
       include: {
